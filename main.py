59c093bb048faa9e6a566006cdf6ac4012af68e4
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-05-08 18:47:05 +0530

    initialize spotify-lyrics-api with basic structure and configuration files

---
+++ main.py
@@ -0,0 +1,84 @@
+import string
+import serial
+import time
+import datetime
+from spotifyFetch import getLyricsData
+
+ser = serial.Serial("COM11", 9600)
+print(ser.name)
+
+
+def findCOM():
+    for i in range():
+        try:
+            ser = serial.Serial(f"COM{i}" , 9600)
+        except:
+            print("error")
+
+
+def encodeStr(str: string):
+    return str.encode("utf-8")
+  
+def sendStr(str):
+  
+  temp = str
+  
+  if(len(temp) > 16):
+    temp = temp[:16] + '\n' + temp[16:]
+    
+  ser.write(encodeStr(temp))
+  
+
+def printStr(str): 
+  print(str)
+
+def sendNumbers():
+  time.sleep(2)
+  sendStr('Starting...')
+  time.sleep(2)
+  i = 0
+  while 1:
+    sendStr(str(i))
+    i += 1
+    time.sleep(1)
+
+def initClock():
+  time.sleep(2)
+  while 1:
+    current_datetime = datetime.datetime.now()
+    formatted_time = current_datetime.strftime("%H:%M:%S")
+    # if(int(formatted_time[0:2]) > 12):
+    #   formatted_time += "PM"
+    # else:
+    #   formatted_time += "AM"
+    sendStr(formatted_time)
+    time.sleep(1)
+  
+
+def initTransfer():
+    while 1:
+        time.sleep(1)
+        sendStr('Wassup!')
+
+def startLyricsTransfer():
+  lyrics = getLyricsData('https://open.spotify.com/track/0AAMnNeIc6CdnfNU85GwCH?si=da2afe08a6724759')
+  print('fetched....')  
+  for i in lyrics:
+    time.sleep(i['startTime'])
+    
+    sentence = i['words']
+    
+    for()
+    
+    sendStr(sentence)
+    print(i['words'])
+
+
+# sendNumbers()
+
+############################# START HERE #############################
+# getLyricsData()
+# initClock()
+# print('start!')
+startLyricsTransfer()
+# time.sleep(2)
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'akashrchandran/spotify-lyrics-api' => array(
+            'pretty_version' => 'v2.1.1',
+            'version' => '2.1.1.0',
+            'reference' => '8b037155700ef1763fe6e16edcc65fd333f5b88b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../akashrchandran/spotify-lyrics-api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
